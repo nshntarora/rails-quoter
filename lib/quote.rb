0bf28e907e798acd0515065f43695eb1489d274a
Martin Kangur <martin@cariba.ee>
2016-10-13 23:36:01 +0300

    inline template and rails5

--- lib/quote.rb
+++ lib/quote.rb
@@ -1,12 +1,19 @@
 require 'quoteDB'
 
 module Quote
-  def quote_something
+  def quote_something(template: :multiline)
   	qcount=QuoteDB.quotes.count-1;
-	n = rand(qcount)
-	quote=	QuoteDB.quotes[n][0]
-	quotee= QuoteDB.quotes[n][1]
-	q="<div class='quoter'><div class='quote'>"+quote+"</div><div class='quotee'>"+quotee+"</div></div>"
+		n = rand(qcount)
+		quote=	QuoteDB.quotes[n][0]
+		quotee= QuoteDB.quotes[n][1]
+		case template.to_sym
+		when :inline
+			q="<div class='quote'>#{quote} - #{quotee}</div>"
+		else
+			q="<div class='quoter'><div class='quote'>"+quote+"</div><div class='quotee'>"+quotee+"</div></div>"
+		end
   	return q.html_safe
   end
+
+
 end
